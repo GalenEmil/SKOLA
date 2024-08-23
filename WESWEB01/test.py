9cca17b7f91ef00bda7fff55420c9543314af750
GalenEmil <emil.bonevnilsson@elev.ntig.ga.se>
2024-08-23 10:40:39 +0200

    Lektion1

---
+++ WESWEB01/test.py
@@ -0,0 +1,11 @@
+# Skämt om Niord
+print("WASSAH Lieutenant")
+
+# Skämt om Niord
+print("WASSAH Lieutenant")
+
+# Fråga om namn
+namn = input("Vad heter du? ")
+
+# Hälsa på namnet
+print("Hej", namn)
