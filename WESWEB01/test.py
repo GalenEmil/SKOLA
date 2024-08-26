41800fb9ed03b79fabcb94f722912c14127540ab
GalenEmil <emil.bonevnilsson@elev.ntig.ga.se>
2024-08-26 12:10:05 +0200

    s

--- WESWEB01/test.py
+++ WESWEB01/test.py
@@ -1,6 +1,16 @@
+"""
 dic = {"banan": 1, "äpple": 2, "päron": 3}
 
 print(dic)
 
 for x, y in dic.items():
     print(x, y)
+
+fruits = ["Nektarin", "Persika", "Drakfrukt", "Mango", "Kiwi", "Papaya", "Persilja"]
+
+"""
+list = ["JACOB LEWIS FARNSWORTH IS THE BEST"]
+# OÄNDLIG FOR-LOOP
+for x in list:
+    list.append(x)
+    print(list)
